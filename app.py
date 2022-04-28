10509723c31bbb12b76aeb49ff573989047c9cac
Mohammad Kachmar <mhk51@mail.aub.edu>
2022-04-28 10:43:37 +0300

    Added templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
@@ -25,3 +25,17 @@ from models.models import *
 @app.route('/home',methods = ['GET'])
 def hello_word():
     return 'hello world'
+
+
+@app.route('/signin',methods = ['GET'])
+def render_sign_in():
+    return render_template('sign_in.html')
+
+@app.route('/signin',methods = ['POST'])
+def sign_in():
+    user_email = (request.form['user_email'])
+    password = (request.form['user_password'])
+    if(password == 'whatever'):
+        return render_template('page2.html')
+    else:
+        return render_sign_in()
